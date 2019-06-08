c97f11469eb87f491b75df2d9218ff03472d0c19
pods13 <alta0815@yandex.ru>
2019-06-08 15:39:30 +0300

    Add first basic react component - ProductList

---
+++ public/app.js
@@ -0,0 +1,14 @@
+class ProductList extends React.Component {
+	render() {
+		return (
+			<div className='ui'>
+				Product List
+			</div>
+		);
+	}
+}
+
+ReactDOM.render(
+	<ProductList/>,
+	document.getElementById('content')
+)
